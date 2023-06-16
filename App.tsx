8302b5e373aed9445d4be92545bc9fc1021a5175
bdsilva16 <bd6866190@gmail.com>
2023-06-16 16:57:32 -0300

    tamo na merda

--- App.tsx
+++ App.tsx
@@ -1,14 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 import DepartureEntity from './src/entities/departure_entity';
+import { Image } from 'expo-image';
 
 
+export default function App() {
 
-export default function App({}) {
-
-  const [departure, setDeparture] = useState<DepartureEntity[]>([]);
+  const [departures, setDeparture] = useState<DepartureEntity[]>([]);
 
 
   useEffect(() => {
@@ -20,40 +19,75 @@ export default function App({}) {
       headers: myHeaders
     };
 
-    let departurePosition: DepartureEntity[] = [];
+    let departuresList: DepartureEntity[] = [];
 
-    fetch("https://api.api-futebol.com.br/v1/campeonatos/10/tabela", requestOptions)
+    fetch("https://api.api-futebol.com.br/v1/partidas/10", requestOptions)
       .then(response => response.text())
       .then(result => JSON.parse(result))
       .then(dataJson => {
-        dataJson.map((departure) => {
+        console.log(dataJson)
+        dataJson.map((departure: any) => {
 
-          const dataDeparture = {
-            id: departure['campeonato']['campeonato_id'],
-            team_shield_url:departure[''],
-            team_name:departure[''],
-            scoreboard:departure['placar']
+          const dataDeparture: DepartureEntity = {
 
-
+            partida_id: departure['partida_id'],
+            campeonato: {
+              campeonato_id: departure['campeonato']['campeonato_id'],
+              nome: departure['campeonato']['nome'],
+              slug: departure['campeonato']['slug'],
+              nome_popular: departure['campeonato']['nome_popular'],
+              fase_atual: {
+                fase_id: departure['fase_atual']['fase_id'],
+                nome: departure['fase_atual']['nome'],
+                slug: departure['fase_atual']['slug'],
+                tipo: departure['fase_atual']['tipo']
+                // _link: /v1/campeonatos/10/fases/317
+              }
+            },
+            placar: departure['placar'],
+            time_mandante: {
+              time_id: departure['time_mandante']['time_id'],
+              nome_popular: departure['time_mandante']['nome_popular'],
+              sigla: departure['time_mandante']['sigla'],
+              escudo: departure['time_mandante']['escudo']
+            },
+            time_visitante: {
+              time_id: departure['time_visitante']['time_id'],
+              nome_popular: departure['time_visitante']['nome_popular'],
+              sigla: departure['time_visitante']['sigla'],
+              escudo: departure['time_visitante']['escudo']
+            },
+            placar_mandante: departure['placar_mandante'],
+            placar_visitante: departure['placar_visitante']
           };
 
-          departurePosition.push(dataDeparture);
+
+          departuresList.push(dataDeparture);
         });
-        setDeparture(departurePosition);
-        console.log(departurePosition);
+        setDeparture(departuresList);
+        console.log(departuresList);
       })
       .catch(error => console.log('error', error));
   }, []);
-    
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Champions League</Text>
-
       <View style={styles.card}>
-      <Text style={styles.team_name}>{departure.item.team_name}</Text>
+        <FlatList
+          data={departures}
+          keyExtractor={(item) => item.partida_id.toString()}
+          renderItem={(departure) =>
 
+            <View style={styles.item}>
+              <Text>breno</Text>
+              <Image source={departure.item.time_mandante.escudo}/>
+            </View>
 
+          }
+        />
       </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -82,5 +116,26 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     margin: 30
 
-  }
+  },
+  item: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'flex-start',
+    paddingHorizontal: 5,
+    paddingTop: 8,
+    height: 50
+  },
+  team_name: {
+    fontSize: 20,
+    width: 180,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    color: 'black'
+  },
+  team_shield: {
+    width: 30,
+    height: 30,
+    marginRight: 15
+  },
+
 });
--- src/entities/departure_entity.tsx
+++ src/entities/departure_entity.tsx
@@ -1,6 +1,31 @@
-export default class DepartureEntity{
-    id:number;
-    team_shield_url: string;
-    team_name: string;
-    scoreboard:string;
-}
+export default class DepartureEntity {
+    partida_id: number;
+    campeonato: {
+        campeonato_id: number;
+        nome: string;
+        slug: string;
+        nome_popular: string;
+        fase_atual: {
+            fase_id: number;
+            nome: string;
+            slug: string;
+            tipo: string;
+            // _link: /v1/campeonatos/10/fases/317
+        }
+    };
+    placar: string
+    time_mandante: {
+        time_id: number;
+        nome_popular: string;
+        sigla: string;
+        escudo: string;
+    };
+    time_visitante: {
+        time_id: number;
+        nome_popular: string;
+        sigla: string;
+        escudo: String;
+    };
+    placar_mandante: number;
+    placar_visitante: number
+} 
